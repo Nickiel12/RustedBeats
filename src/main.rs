17e2776ef795abe187b5a25241bec7a76721d81f
Nickiel12 <nicholasyoungsumner@gmail.com>
2022-12-09 14:11:43 -0800

    basic cli usage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about=None)]
+struct Cli {
+    #[arg(short, long)]
+    configuration_file: Option<String>,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     println!("Hello, world!");
+    println!("{:?}", cli);
 }
