a12af938ba991ea5ae45173f1b90363c3a5b958c
Nickiel12 <nicholasyoungsumner@gmail.com>
2022-12-09 13:34:45 -0800

    First commit

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ pkgs ? import <nixpkgs> {}}:
+
+let
+  rust_overlay = import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/master.tar.gz");
+  pkgs = import <nixpkgs> { overlays = [ rust_overlay ]; };
+  ruststable = (pkgs.latest.rustChannels.stable.default.override {
+      extensions = [
+        "rust-src"
+      ];
+    });
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ruststable
+    sqlite
+    sqliteman
+  ];
+
+  RUST_BACKTRACE = 1;
+}
