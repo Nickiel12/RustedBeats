407e61976b8caa8e22eaccba3c72fe9b8e8a44b4
Nickiel12 <nicholasyoungsumner@gmail.com>
2022-12-10 22:31:02 -0800

    docs: Actually added basic docs. Will be update, I swear

--- src/file_operations.rs
+++ src/file_operations.rs
@@ -4,6 +4,20 @@ use scan_dir::ScanDir;
 
 const SUPPORTED_FILETYPES: [&str; 1] = ["mp3"];
 
+/// Recursivly scans the given root directory for files
+/// WIP Function! All it does is print the directories
+///
+/// # Examples
+///
+/// ```rust
+/// scan_music_dir("/home/Documents/RustedBeats");
+///
+///>>> "I found a directory src at .../src"
+///>>> "I found a directory debug at .../debug"
+///>>> "I found a file Cargo.toml at .../Cargo.toml"
+///...
+/// ```
+///
 pub fn scan_music_dir(root: String) -> Result<(), ()> {
     let mut directories = Vec::<PathBuf>::new();
     directories.push(root.into());
